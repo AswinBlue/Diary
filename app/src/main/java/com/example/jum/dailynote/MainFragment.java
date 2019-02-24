205d1a8628e5932b27ad3392473ae5b36d52e198
aswinblue <jepilon@naver.com>
2019-02-24 20:54:22 +0900

    Developing : > adding image as attachments Complete : > fixed fragment_main.xml weights TODO : > adding photos > Calendar view > navigation bar menu settings > memo EVENT treatment

--- app/src/main/java/com/example/jum/dailynote/MainFragment.java
+++ app/src/main/java/com/example/jum/dailynote/MainFragment.java
@@ -8,19 +8,24 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.ExifInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -53,10 +58,13 @@ import com.google.api.services.calendar.model.Calendar;
 import com.google.api.services.calendar.model.CalendarList;
 import com.google.api.services.calendar.model.CalendarListEntry;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttachment;
 import com.google.api.services.calendar.model.EventDateTime;
 import com.google.api.services.calendar.model.Events;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -80,11 +88,11 @@ public class MainFragment extends Fragment implements EasyPermissions.Permission
     private com.google.api.services.calendar.Calendar mService = null;
     GoogleAccountCredential mCredential;
 
-    static final int REQUEST_IMAGE = 100;
     static final int REQUEST_ACCOUNT_PICKER = 1000;
     static final int REQUEST_AUTHORIZATION = 1001;
     static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
     static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+    static final int REQUEST_IMAGE_GALLARY = 1004;
 
     private static final String PREF_ACCOUNT_NAME = "accountName";
     private static final String[] SCOPES = {CalendarScopes.CALENDAR};
@@ -115,6 +123,8 @@ public class MainFragment extends Fragment implements EasyPermissions.Permission
     DatePickerDialog date_dialog;
     TimePickerDialog time_dialog1;
     TimePickerDialog time_dialog2;
+
+    ArrayList<Uri> file_to_attach = new ArrayList<Uri>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -217,7 +227,7 @@ public class MainFragment extends Fragment implements EasyPermissions.Permission
                         Intent intent = new Intent();
                         intent.setType("image/*");
                         intent.setAction(Intent.ACTION_GET_CONTENT);
-                        startActivityForResult(intent, REQUEST_IMAGE); // onActivityResult를 자동으로 호출
+                        startActivityForResult(intent, REQUEST_IMAGE_GALLARY); // onActivityResult를 자동으로 호출
                     } else {
                         ActivityCompat.requestPermissions(getActivity(),
                                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
@@ -279,6 +289,33 @@ public class MainFragment extends Fragment implements EasyPermissions.Permission
         }
     };
 
+    //***********************
+    //***** picture treatment
+    //***********************
+    private String getRealPathFromURI(Uri contentUri) {
+        int column_index=0;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContext().getContentResolver().query(contentUri, proj, null, null, null);
+        if(cursor.moveToFirst()){
+            column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        }
+
+        return cursor.getString(column_index);
+    }
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    //**********************
+    //***** my own functions
+    //**********************
     private void saveDiary(String calendarTitle) {
         // save diary
         String calendarID = getCalendarID(calendarTitle);
@@ -308,8 +345,6 @@ public class MainFragment extends Fragment implements EasyPermissions.Permission
                 .setTimeZone("Asia/Seoul");
         event.setStart(start);
 
-
-
         datetime = simpledateformat.format(end_date);
         Log.d("@@@", datetime);
         DateTime endDateTime = new DateTime(datetime);
@@ -334,6 +369,25 @@ public class MainFragment extends Fragment implements EasyPermissions.Permission
 
     }//->saveDiary
 
+    private void addAttachments(Event event){
+        List<EventAttachment> attachments = event.getAttachments();
+        if (attachments == null) {
+            attachments = new ArrayList<EventAttachment>();
+        }
+
+        ContentResolver cr = getActivity().getContentResolver();
+        for(Uri uri : file_to_attach) {
+            try {
+                attachments.add(new EventAttachment()
+                        .setFileUrl((new URL(uri.toString())).toString())
+                        .setMimeType(cr.getType(uri))
+                        .setTitle(uri.getPath())
+                );
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     //***************
     //*****Google GPS
@@ -588,14 +642,26 @@ public class MainFragment extends Fragment implements EasyPermissions.Permission
                 }
                 break;
 
-            case REQUEST_IMAGE:
+            case REQUEST_IMAGE_GALLARY:
                 if (resultCode == RESULT_OK){
                     // TODO: 이미지 넣는 코드
+                    file_to_attach.add(data.getData());
+                    /*
+                    String imagePath = getRealPathFromURI(data.getData());
+                    ExifInterface exif = null;
+                    try {
+                        exif = new ExifInterface(imagePath);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    int exifDegree = exifOrientationToDegrees(exifOrientation);
+*/
+
                 }
                 break;
         }
     }
-
 
     /*
      * Android 6.0 (API 23) 이상에서 런타임 권한 요청시 결과를 리턴받음
